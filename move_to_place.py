3c6443658f99994b7f154ec10837ca8b0ced3882
ChidiebereMicah <111301657+ChidiebereMicah@users.noreply.github.com>
2024-06-15 13:53:16 +0100

    Update move_to_place.py

--- move_to_place.py
+++ move_to_place.py
@@ -15,76 +15,7 @@ class MoveToPlaceNode(Node):
         #the turtles arfe to navigate to the point x:8 and y:8 in the turtlesim simulator
         self.target_x = 8.0
         self.target_y = 8.0
-
-    '''
-    #Trial 1
-    def pose_callback(self, pose:Pose):
-        cmd = Twist()
-        self.get_logger().info(str(pose.x) + ' ' + str(pose.theta))
-
-        #To move the turtle along the x-axis
-        if pose.x <= 9.0:
-            while pose.theta != 0:
-                cmd.angular.z = 0.5
-            while pose.x < 9.0:
-                cmd.linear.x = 1.0
-        else:
-            while pose.theta != 22/7:
-                cmd.angular.z = 0.5
-            while pose.x > 9.0:
-                cmd.linear.x = 1.0
-
-        #To move the turtle along the y-axis 
-        if pose.y <= 9.0:
-            while pose.theta != 22/14:
-                cmd.angular.z = 0.5
-            while pose.y < 9.0:
-                cmd.linear.x = 0.7
-        else:
-            while pose.theta != 33/7:
-                cmd.angular.z = 0.5
-            while pose.y > 9.0: 
-                cmd.linear.x = 1.0
-        #cmd.angular.z = 0.5
-
-        self.cmd_vel_publisher.publish(cmd)
-
-
-    #Trial 2
-    def pose_callback(self, pose: Pose):
-        cmd = Twist()
-        self.get_logger().info(f'Current position: x={pose.x}, theta={pose.theta}')
         
-        # Rotate to face the correct direction for x movement
-        if pose.x < self.target_x:
-            if abs(pose.theta) > 0.1:  # Small tolerance to avoid infinite loop
-                cmd.angular.z = 0.01
-        elif pose.x > self.target_x:
-            if abs(pose.theta - math.pi) > 0.1:  # Small tolerance for 180 degrees
-                cmd.angular.z = 0.01
-
-        # Move along x-axis
-        if pose.x < self.target_x:
-            cmd.linear.x = 0.05
-        elif pose.x > self.target_x:
-            cmd.linear.x = 0.05
-        
-        # Rotate to face the correct direction for y movement
-        if pose.y < self.target_y:
-            if abs(pose.theta - math.pi/2) > 0.1:  # Small tolerance for 90 degrees
-                cmd.angular.z = 0.01
-        elif pose.y > self.target_y:
-            if abs(pose.theta - 3*math.pi/2) > 0.1:  # Small tolerance for 270 degrees
-                cmd.angular.z = 0.01
-
-        # Move along y-axis
-        if pose.y < self.target_y:
-            cmd.linear.x = 0.05
-        elif pose.y > self.target_y:
-            cmd.linear.x = 0.05
-        
-        self.cmd_vel_publisher.publish(cmd)'''
-
     def pose_callback(self, pose: Pose):
         cmd = Twist()
         self.get_logger().info(f'Current position: x={pose.x}, theta={pose.theta}')
